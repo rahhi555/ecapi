5c7d58e2fca38e858624bd6de012693820ec0c40
rahhi555 <68116815+rahhi555@users.noreply.github.com>
2023-07-28 19:23:10 +0900

    init commit

---
+++ src/main/java/com/example/ecapi/EcapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcapiApplication.class, args);
+	}
+
+}
