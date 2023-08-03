49cec62fd175cff7106027cc513d85c93c3df9e6
rahhi555 <68116815+rahhi555@users.noreply.github.com>
2023-08-04 02:09:42 +0900

    Add: CORS追加

--- src/main/java/com/example/ecapi/EcapiApplication.java
+++ src/main/java/com/example/ecapi/EcapiApplication.java
@@ -1,29 +1,13 @@
 package com.example.ecapi;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EcapiApplication {
-
-	@Value("${cors.allowedOrigins}")
-	private String allowedOrigins;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcapiApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigure() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins(allowedOrigins);
-			}
-		};
-	}
 }
