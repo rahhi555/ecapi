716838cedb6b67736dca7664bdfbe542346dbf95
rahhi555 <68116815+rahhi555@users.noreply.github.com>
2023-08-13 13:51:19 +0900

    Add: storage

---
+++ src/main/java/com/example/ecapi/config/StorageProperties.java
@@ -0,0 +1,19 @@
+package com.example.ecapi.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("storage")
+public class StorageProperties {
+    /**
+    * Folder location for storing files
+    */
+    private String location = "storage";
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
